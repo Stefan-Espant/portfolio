0d633626507bf353bdd765ad8045d182f4736c9d
Stefan Espant <stefan.van.der.kort@hva.nl>
2024-01-26 13:19:24 +0100

    🎉 new portfolio

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Portfolio van Stefan',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  }
+})
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,15 @@
+<script>
+    // Import the global stylesheet
+    import '~/assets/styles/global.css'
+</script>
+
+
+<template>
+    <div>
+        <h1>test</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
