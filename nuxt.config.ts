d8c2d5ab1f8dd373f192a3d331329f3773c8c42f
Stefan1210 <stefan.van.der.kort@hva.nl>
2024-05-16 14:05:49 +0200

    removed comments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,9 @@ export default defineNuxtConfig({
 		}
 	},
 	routeRules: {
-		// all routes (by default) will be revalidated every 60 seconds, in the background
 		'/**': { isr: 60 },
-		// this page will be generated on demand and then cached permanently
 		'/static': { isr: true },
-		// this page is statically generated at build time and cached permanently
 		'/prerendered': { prerender: true },
-		// this page will be always fresh
 		'/dynamic': { isr: false },
 	  },
 	app: {
