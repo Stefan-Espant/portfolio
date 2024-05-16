0f827ce1c62c0e2235c886456d8485e12300c74f
Stefan1210 <stefan.van.der.kort@hva.nl>
2024-05-16 14:05:09 +0200

    make my site static generated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,16 @@ export default defineNuxtConfig({
 			githubToken: process.env.GITHUB_TOKEN
 		}
 	},
+	routeRules: {
+		// all routes (by default) will be revalidated every 60 seconds, in the background
+		'/**': { isr: 60 },
+		// this page will be generated on demand and then cached permanently
+		'/static': { isr: true },
+		// this page is statically generated at build time and cached permanently
+		'/prerendered': { prerender: true },
+		// this page will be always fresh
+		'/dynamic': { isr: false },
+	  },
 	app: {
 		head: {
 			htmlAttrs: {
