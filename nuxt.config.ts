8c4778505ec4baaf0b03a27032626907f2df8173
Stefan1210 <stefan.van.der.kort@hva.nl>
2024-07-05 19:38:03 +0200

    changed the format of the favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 			charset: 'utf-8',
 			viewport: 'width=device-width, initial-scale=1',
 			link: [
-				{ rel: "icon", type: "image/png", href: "/assets/images/favicon.png" }
+				{ rel: "icon", type: "image/png", href: "/assets/images/favicon.ico" }
 			  ]
 		},
 	},
